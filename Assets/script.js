868c5dddb3f1521a8866abb22d83a101ab7e7f1c
paullsshin <paullsshin@hotmail.com>
2023-01-05 16:08:02 -0800

    added changes to css and added some comments to the files

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,5 @@
+// created variables for each time slot
+
 var hr9 = $("#9");
 var hr10 = $("#10");
 var hr11 = $("#11");
@@ -9,10 +11,11 @@ var hr4 = $("#16");
 var hr5 = $("#17");
 var time = moment();
 
+
 function beginPlanner() {
-
+// this give us the current day month year and time
     $("#currentDay").text(moment().format("dddd, MMMM DD, YYYY h:m:ss"));
-
+// this portion is getting the local storage item
     $(".time-block").each(function () {
         var id = $(this).attr("id");
         var schedule = localStorage.getItem(id);
@@ -25,14 +28,16 @@ function beginPlanner() {
 
 beginPlanner();
 var saveBtn = $(".saveBtn");
-
+// once the save button is clicked it will save it to local storage with whatever is inside the container
 saveBtn.on("click", function () {
     var time = $(this).parent().attr("id");
     var schedule = $(this).siblings(".schedule").val();
 
     localStorage.setItem(time, schedule);
 });
-
+// with this function, once the time slot has passed will change the color of the time block to gray
+// when the time slot is in the future it will make the time block green
+// current time block will be displayed as the current/present time
 function timeBlock() {
     hour = time.hours();
     $(".time-block").each(function () {
